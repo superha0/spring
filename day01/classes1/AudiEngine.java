78e00ababe7de1dc7b9c5e4cd1a1c632aaebfdde
superha0 <j2onpro@naver.com>
2019-01-28 22:16:30 +0900

    day01

---
+++ day01/classes1/AudiEngine.java
@@ -0,0 +1,17 @@
+package kr.co.spring.day01.classes1;
+
+public class AudiEngine extends Engine{
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		System.out.println("아우디 엔진 시동 걸기");
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		System.out.println("아우디 엔진 시동 끄기");
+	}
+
+}
