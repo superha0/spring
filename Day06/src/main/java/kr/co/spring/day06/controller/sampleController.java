803fcb7f7c0ed0d1bce3f52a6885017cb89c6286
superha0 <j2onpro@naver.com>
2019-02-14 22:19:17 +0900

    MyBoard

--- Day06/src/main/java/kr/co/spring/day06/controller/sampleController.java
+++ Day06/src/main/java/kr/co/spring/day06/controller/sampleController.java
@@ -3,11 +3,13 @@ package kr.co.spring.day06.controller;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.co.spring.day06.domain.SampleDTO;
 import lombok.extern.log4j.Log4j;
@@ -72,5 +74,74 @@ public class sampleController {
 		log.info("ids : " + ids);
 		return "ex02List";
 	}
+
+	
+	
+	//서버 클라이언트에게 데이터를 전달 할 때는
+	// 반드시 Model 객체를 메소드의 매개변수로 넣어줘야 한다.
+	@GetMapping("/ex03")
+	public String ex03(Model model) {
+		//전달할 데이터를 addAttribute 메소드를 이용해 넣어준다.
+		model.addAttribute("name", "jhadsf");
+		model.addAttribute("age",30);
+		
+		return "ex03"; //ex03.jsp 로 포워딩 (forward)
+	}
+	
+	
+	
+	//JAVA Bean의 자동 어트리뷰트 전달
+	// *Java bean 규칙
+	// 1) 생성자에 매개변수가 없어야 한다. (보통 기본 생성자만 이용)
+	// 2) 메소드 getter/setter 만 있어야 한다.
+	
+	// *Java Bean 은 Spring Framework 에서 자동으로 만들어주는 객체
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO sampleDTO, @ModelAttribute("page") int page) {
+		//@ModelAttribute : 매개변수로 변수를 attribute 로 등록시켜줌
+		log.info("dto : " + sampleDTO); // sampleDTO 가 자동으로 어트리뷰트가 된다,
+		log.info("page: " + page);
+		
+		//sampleDTO가 자동으로 어트리뷰트로 등록되어서 ex04에 전달됨.
+		//이 때 key값은 클래스의 앞글자(SampleDTO)를 
+		// 소문자로 만든 형태(sampleDTO)로 지정됨
+		
+		return "ex04";
+	}
+	
+	
+	//Controller 메소드의 리턴타입
+	// void, String
+	// VO, DTO => 주로 JSON 타입을 생성할 때 사용함 (RestFul 개발)
+	//ResponseEntity => HTTP 헤더 정보와 내용을 가공
+	//	* 에러 코드 설정 또는 응답 (response)에 대한 내용을 직접 가공
+	// Model, ModelAndView : 모델로 데이터를 반환하거나 
+	// 모델과 화면을 동시에 보여주는 경우에 사용 (최근에는 잘 사용 안함)
+	// HttpHeaders : 응답에 내용이 없이 단순 헤더 메시지만 전달
+	
+	
+	// 메소드가 void 타입일 경우
+	//url 의 이름을 jsp 로 활용
+	
+	public void ex05() {
+		log.info("ex05.........................");
+	}
+	
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("ex06.........................");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(30);
+		dto.setName("aaa");
+		
+		return dto;
+	}
+	
+	
+	
+	
+	
 	
 }
---
+++ Day06/src/main/webapp/WEB-INF/views/ex04.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>전달된 SampleDTO : ${sampleDTO} </h1> <!-- 클래스와 맨 앞글자를 소문자로 하면 key 값이 된다. -->
+	<h1>전달된 page: ${page}</h1>
+</body>
+</html>
